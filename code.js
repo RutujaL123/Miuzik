90533db006520757f90b61cb95e828be4e1e11e5
RutujaL123 <96522053+RutujaL123@users.noreply.github.com>
2022-02-03 07:49:14 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,399 @@
+onEvent("let'sgoBUTTON", "click", function( ) {
+  setScreen("screen1SONGS");
+});
+onEvent("button3", "click", function( ) {
+  setScreen("screen2SONGS");
+});
+onEvent("button1", "click", function( ) {
+  setScreen("screen3SONGS");
+});
+onEvent("button7", "click", function( ) {
+  setScreen("screen4SONGS");
+});
+onEvent("button12", "click", function( ) {
+  setScreen("screen5SONGS");
+});
+onEvent("button2", "click", function( ) {
+  setScreen("screen6SONGS");
+});
+onEvent("button9", "click", function( ) {
+  setScreen("screen7SONGS");
+});
+onEvent("button5", "click", function( ) {
+  setScreen("screen6SONGS");
+});
+onEvent("button6", "click", function( ) {
+  setScreen("screen5SONGS");
+});
+onEvent("button8", "click", function( ) {
+  setScreen("screen4SONGS");
+});
+onEvent("button10", "click", function( ) {
+  setScreen("screen3SONGS");
+});
+onEvent("button11", "click", function( ) {
+  setScreen("screen2SONGS");
+});
+onEvent("button13", "click", function( ) {
+  setScreen("screen1SONGS");
+});
+onEvent("DARKSIDEbutton1", "click", function( ) {
+  setScreen("Darkside");
+});
+onEvent("FADEDbutton2", "click", function( ) {
+  setScreen("Faded");
+});
+onEvent("SWEETBUTPSYCHObutton3", "click", function( ) {
+  setScreen("SweetButPsycho");
+});
+onEvent("ONELASTTIMEbutton4", "click", function( ) {
+  setScreen("OneLastTime");
+});
+onEvent("CANTSTOPTHEFEELINGbutton", "click", function( ) {
+  setScreen("Can'tStoptheFeeling");
+});
+onEvent("WEDONTTALKANYMOREbutton6", "click", function( ) {
+  setScreen("WeDontTalkAnymore");
+});
+onEvent("CHEAPTHRILLSbutton7", "click", function( ) {
+  setScreen("CheapThrills");
+});
+onEvent("LETMELOVEYOUbutton8", "click", function( ) {
+  setScreen("LetmeLoveyou");
+});
+onEvent("WATERMELONSUGARbutton9", "click", function( ) {
+  setScreen("WatermelonSugar");
+});
+onEvent("THUNDERbutton10", "click", function( ) {
+  setScreen("Thunder");
+});
+onEvent("SORRYbutton11", "click", function( ) {
+  setScreen("Sorry");
+});
+onEvent("FIREWORKSbutton12", "click", function( ) {
+  setScreen("Fireworks");
+});
+onEvent("MEMORIESbutton13", "click", function( ) {
+  setScreen("Memories");
+});
+onEvent("WHATMAKESYOUBEAUTIFULbutton14", "click", function( ) {
+  setScreen("WhatmakesYOUbeautiful");
+});
+onEvent("PARTYINTHEUSAbutton15", "click", function( ) {
+  setScreen("PartyINtheUSA");
+});
+onEvent("CIRCLESbutton16", "click", function( ) {
+  setScreen("Circles");
+});
+onEvent("SUNFLOWERbutton17", "click", function( ) {
+  setScreen("Sunflower");
+});
+onEvent("FIGHTSONGbutton18", "click", function( ) {
+  setScreen("FightSong");
+});
+onEvent("ROARbutton19", "click", function( ) {
+  setScreen("Roar");
+});
+onEvent("SCARSTOYOURBEAUTIFULbutton20", "click", function( ) {
+  setScreen("ScarsToYourBeautiful");
+});
+onEvent("IKNEWYOUWERETROUBLEbutton21", "click", function( ) {
+  setScreen("IknewYouweretrouble");
+});
+onEvent("CLOSERbutton22", "click", function( ) {
+  setScreen("Closer");
+});
+onEvent("AMILLIONDREAMSbutton23", "click", function( ) {
+  setScreen("AMillionDreams");
+});
+onEvent("BLINDINGLIGHTSbutton24", "click", function( ) {
+  setScreen("BlindingLights");
+});
+onEvent("THEMIDDLEbutton25", "click", function( ) {
+  setScreen("TheMiddle");
+});
+onEvent("TRYEVERYTHINGbutton26", "click", function( ) {
+  setScreen("TryEverything");
+});
+onEvent("DONTSTARTNOWbutton27", "click", function( ) {
+  setScreen("DontStartNow");
+});
+onEvent("COUNTINGSTARSbutton28", "click", function( ) {
+  setScreen("CountingStars");
+});
+onEvent("Play1", "click", function( ) {
+  playSound("Alan-Walker---Darkside-(Lyrics)-ft.-Au_Ra-and-Tomine-Harket.mp3", true);
+});
+onEvent("Stop1", "click", function( ) {
+  stopSound("Alan-Walker---Darkside-(Lyrics)-ft.-Au_Ra-and-Tomine-Harket.mp3");
+});
+onEvent("Play2", "click", function( ) {
+  playSound("Alan-Walker---Faded-(Lyrics).mp3", true);
+});
+onEvent("Stop2", "click", function( ) {
+  stopSound("Alan-Walker---Faded-(Lyrics).mp3");
+});
+onEvent("Play3", "click", function( ) {
+  playSound("Ava-Max---Sweet-but-Psycho-(Lyrics).mp3", true);
+});
+onEvent("Stop3", "click", function( ) {
+  stopSound("Ava-Max---Sweet-but-Psycho-(Lyrics).mp3");
+});
+onEvent("Play4", "click", function( ) {
+  playSound("Ariana-Grande---One-Last-Time-(Lyric-Video).mp3", true);
+});
+onEvent("Stop4", "click", function( ) {
+  stopSound("Ariana-Grande---One-Last-Time-(Lyric-Video).mp3");
+});
+onEvent("Play5", "click", function( ) {
+  playSound("CAN'T-STOP-THE-FEELING!---Justin-Timberlake-(Lyrics)--.mp3", true);
+});
+onEvent("Stop5", "click", function( ) {
+  stopSound("CAN'T-STOP-THE-FEELING!---Justin-Timberlake-(Lyrics)--.mp3");
+});
+onEvent("Play6", "click", function( ) {
+  playSound("Charlie-Puth---We-Don't-Talk-Anymore-(Lyrics)-Ft.-Selena-Gomez.mp3", true);
+});
+onEvent("Stop6", "click", function( ) {
+  stopSound("Charlie-Puth---We-Don't-Talk-Anymore-(Lyrics)-Ft.-Selena-Gomez.mp3");
+});
+onEvent("Play7", "click", function( ) {
+  playSound("assets/Cheap-Thrills-audio-track.mp3", true);
+});
+onEvent("Stop7", "click", function( ) {
+  stopSound("assets/Cheap-Thrills-audio-track.mp3");
+});
+onEvent("Play8", "click", function( ) {
+  playSound("assets/DJ-Snake-ft.-Justin-Bieber---Let-Me-Love-You--Lyric-Video-.mp3", true);
+});
+onEvent("Stop8", "click", function( ) {
+  stopSound("assets/DJ-Snake-ft.-Justin-Bieber---Let-Me-Love-You--Lyric-Video-.mp3");
+});
+onEvent("Play9", "click", function( ) {
+  playSound("Harry-Styles---Watermelon-Sugar-(Lyrics).mp3", true);
+});
+onEvent("Stop9", "click", function( ) {
+  stopSound("Harry-Styles---Watermelon-Sugar-(Lyrics).mp3");
+});
+onEvent("Play10", "click", function( ) {
+  playSound("Imagine-Dragons---Thunder-(Lyrics).mp3", true);
+});
+onEvent("Stop10", "click", function( ) {
+  stopSound("Imagine-Dragons---Thunder-(Lyrics).mp3");
+});
+onEvent("Play11", "click", function( ) {
+  playSound("Justin-Bieber---Sorry-(Lyrics).mp3", true);
+});
+onEvent("Stop11", "click", function( ) {
+  stopSound("Justin-Bieber---Sorry-(Lyrics).mp3");
+});
+onEvent("Play12", "click", function( ) {
+  playSound("Katy-Perry---Firework-(Lyrics).mp3", true);
+});
+onEvent("Stop12", "click", function( ) {
+  stopSound("Katy-Perry---Firework-(Lyrics).mp3");
+});
+onEvent("Play13", "click", function( ) {
+  playSound("Maroon-5---Memories-(Lyrics).mp3", true);
+});
+onEvent("Stop13", "click", function( ) {
+  stopSound("Maroon-5---Memories-(Lyrics).mp3");
+});
+onEvent("Play14", "click", function( ) {
+  playSound("One-Direction---What-Makes-You-Beautiful(Lyrics)-.mp3", true);
+});
+onEvent("Stop14", "click", function( ) {
+  stopSound("One-Direction---What-Makes-You-Beautiful(Lyrics)-.mp3");
+});
+onEvent("Play15", "click", function( ) {
+  playSound("Party-In-The-USA---Miley-Cyrus-(Lyrics)--.mp3", true);
+});
+onEvent("Stop15", "click", function( ) {
+  stopSound("Party-In-The-USA---Miley-Cyrus-(Lyrics)--.mp3");
+});
+onEvent("Play16", "click", function( ) {
+  playSound("Post-Malone---Circles-(Lyrics).mp3", true);
+});
+onEvent("Stop16", "click", function( ) {
+  stopSound("Post-Malone---Circles-(Lyrics).mp3");
+});
+onEvent("Play17", "click", function( ) {
+  playSound("Post-Malone--Swae-Lee---Sunflower-(Lyrics)-(Spider-Man_-Into-the-Spider-Verse).mp3", true);
+});
+onEvent("Stop17", "click", function( ) {
+  stopSound("Maroon-5---Memories-(Lyrics).mp3");
+});
+onEvent("Play18", "click", function( ) {
+  playSound("Rachel-Platten---Fight-Song-(Lyrics).mp3", true);
+});
+onEvent("Stop18", "click", function( ) {
+  stopSound("Rachel-Platten---Fight-Song-(Lyrics).mp3");
+});
+onEvent("Play19", "click", function( ) {
+  playSound("Roar---Katy-Perry-(Lyrics).mp3", true);
+});
+onEvent("Stop19", "click", function( ) {
+  stopSound("Roar---Katy-Perry-(Lyrics).mp3");
+});
+onEvent("Play20", "click", function( ) {
+  playSound("Post-Malone---Circles-(Lyrics).mp3", true);
+});
+onEvent("Stop20", "click", function( ) {
+  stopSound("Post-Malone---Circles-(Lyrics).mp3");
+});
+onEvent("Play21", "click", function( ) {
+  playSound("Taylor-Swift---I-Knew-You-Were-Trouble--(Lyrics).mp3", true);
+});
+onEvent("Stop21", "click", function( ) {
+  stopSound("Taylor-Swift---I-Knew-You-Were-Trouble--(Lyrics).mp3");
+});
+onEvent("Play22", "click", function( ) {
+  playSound("The-Chainsmokers---Closer-(Lyrics)-ft.-Halsey.mp3", true);
+});
+onEvent("Stop22", "click", function( ) {
+  stopSound("The-Chainsmokers---Closer-(Lyrics)-ft.-Halsey.mp3");
+});
+onEvent("Play23", "click", function( ) {
+  playSound("The-Greatest-Showman---A-Million-Dreams-(Lyric-Video)-HD.mp3", true);
+});
+onEvent("Stop23", "click", function( ) {
+  stopSound("The-Greatest-Showman---A-Million-Dreams-(Lyric-Video)-HD.mp3");
+});
+onEvent("Play24", "click", function( ) {
+  playSound("The-Weeknd---Blinding-Lights-(Lyrics).mp3", true);
+});
+onEvent("Stop24", "click", function( ) {
+  stopSound("The-Weeknd---Blinding-Lights-(Lyrics).mp3");
+});
+onEvent("Play21", "click", function( ) {
+  playSound("Zedd--Grey---The-Middle-(Lyrics)-ft.-Maren-Morris.mp3", true);
+});
+onEvent("Stop21", "click", function( ) {
+  stopSound("Zedd--Grey---The-Middle-(Lyrics)-ft.-Maren-Morris.mp3");
+});
+onEvent("Play22", "click", function( ) {
+  playSound("Zootopia---Try-Everything-(Lyrics--Shakira).mp3", true);
+});
+onEvent("Stop22", "click", function( ) {
+  stopSound("The-Chainsmokers---Closer-(Lyrics)-ft.-Halsey.mp3");
+});
+onEvent("Play23", "click", function( ) {
+  playSound("The-Greatest-Showman---A-Million-Dreams-(Lyric-Video)-HD.mp3", true);
+});
+onEvent("Stop23", "click", function( ) {
+  stopSound("The-Greatest-Showman---A-Million-Dreams-(Lyric-Video)-HD.mp3");
+});
+onEvent("Play24", "click", function( ) {
+  playSound("The-Weeknd---Blinding-Lights-(Lyrics).mp3", true);
+});
+onEvent("Stop24", "click", function( ) {
+  stopSound("The-Weeknd---Blinding-Lights-(Lyrics).mp3");
+});
+onEvent("Play25", "click", function( ) {
+  playSound("Zedd--Grey---The-Middle-(Lyrics)-ft.-Maren-Morris.mp3", true);
+});
+onEvent("Stop25", "click", function( ) {
+  stopSound("Zedd--Grey---The-Middle-(Lyrics)-ft.-Maren-Morris.mp3");
+});
+onEvent("Play26", "click", function( ) {
+  playSound("Zootopia---Try-Everything-(Lyrics--Shakira).mp3", true);
+});
+onEvent("Stop26", "click", function( ) {
+  stopSound("Zootopia---Try-Everything-(Lyrics--Shakira).mp3");
+});
+onEvent("Play27", "click", function( ) {
+  playSound("Dua-Lipa---Don't-Start-Now-(Lyrics).mp3", true);
+});
+onEvent("Stop27", "click", function( ) {
+  stopSound("Dua-Lipa---Don't-Start-Now-(Lyrics).mp3");
+});
+onEvent("Play28", "click", function( ) {
+  playSound("OneRepublic---Counting-Stars-(Lyrics).mp3", true);
+});
+onEvent("Stop28", "click", function( ) {
+  stopSound("OneRepublic---Counting-Stars-(Lyrics).mp3");
+});
+onEvent("AnotherSONG1", "click", function( ) {
+  setScreen("screen1SONGS");
+});
+onEvent("AnotherSONG2", "click", function( ) {
+  setScreen("screen1SONGS");
+});
+onEvent("AnotherSONG3", "click", function( ) {
+  setScreen("screen1SONGS");
+});
+onEvent("AnotherSONG4", "click", function( ) {
+  setScreen("screen1SONGS");
+});
+onEvent("AnotherSONG5", "click", function( ) {
+  setScreen("screen2SONGS");
+});
+onEvent("AnotherSONG6", "click", function( ) {
+  setScreen("screen2SONGS");
+});
+onEvent("AnotherSONG7", "click", function( ) {
+  setScreen("screen2SONGS");
+});
+onEvent("AnotherSONG8", "click", function( ) {
+  setScreen("screen2SONGS");
+});
+onEvent("AnotherSONG9", "click", function( ) {
+  setScreen("screen3SONGS");
+});
+onEvent("AnotherSONG10", "click", function( ) {
+  setScreen("screen3SONGS");
+});
+onEvent("AnotherSONG11", "click", function( ) {
+  setScreen("screen3SONGS");
+});
+onEvent("AnotherSONG12", "click", function( ) {
+  setScreen("screen3SONGS");
+});
+onEvent("AnotherSONG13", "click", function( ) {
+  setScreen("screen4SONGS");
+});
+onEvent("AnotherSONG14", "click", function( ) {
+  setScreen("screen4SONGS");
+});
+onEvent("AnotherSONG15", "click", function( ) {
+  setScreen("screen4SONGS");
+});
+onEvent("AnotherSONG16", "click", function( ) {
+  setScreen("screen4SONGS");
+});
+onEvent("AnotherSONG17", "click", function( ) {
+  setScreen("screen5SONGS");
+});
+onEvent("AnotherSONG18", "click", function( ) {
+  setScreen("screen5SONGS");
+});
+onEvent("AnotherSONG19", "click", function( ) {
+  setScreen("screen5SONGS");
+});
+onEvent("AnotherSONG20", "click", function( ) {
+  setScreen("screen5SONGS");
+});
+onEvent("AnotherSONG21", "click", function( ) {
+  setScreen("screen6SONGS");
+});
+onEvent("AnotherSONG22", "click", function( ) {
+  setScreen("screen6SONGS");
+});
+onEvent("AnotherSONG23", "click", function( ) {
+  setScreen("screen6SONGS");
+});
+onEvent("AnotherSONG24", "click", function( ) {
+  setScreen("screen6SONGS");
+});
+onEvent("AnotherSONG25", "click", function( ) {
+  setScreen("screen7SONGS");
+});
+onEvent("AnotherSONG26", "click", function( ) {
+  setScreen("screen7SONGS");
+});
+onEvent("AnotherSONG27", "click", function( ) {
+  setScreen("screen7SONGS");
+});
+onEvent("AnotherSONG28", "click", function( ) {
+  setScreen("screen7SONGS");
+});
